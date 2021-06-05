fd2ecc00d680bc87be6a02425a4d4893fd7c89a0
sanaa-almoghraby <sanaa.almoghraby@gmail.com>
2021-06-06 02:28:42 +0300

    301-exam

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 'use strict'
-let imageofPlace = ['HAWAII', 'LONDON', 'PARIS', 'ITALY', 'SLOVAKIA', 'MALAYSIA']
 let allPlaces = [];
-function AllPlaces(pname, tplace, toftransport) {
+function NewPlace(pname, tplace, toftransport) {
     this.pname = pname;
     this.tplace = tplace;
     this.toftransport = toftransport;
@@ -9,6 +8,7 @@ function AllPlaces(pname, tplace, toftransport) {
 }
 function renderTravel() {
     getFromlocal();
+    clearlist();
     showListofPlace();
 }
 
@@ -18,10 +18,12 @@ submit.addEventListener('click', addNewplaces);
 function addNewplaces(event) {
     event.preventDefault();
     let placename = document.getElementById('placename').value;
-    let selectoption = document.getElementById('selectoption').value
-    let typeoftransport = document.getElementById('typeoftransport').value;
 
-    new AllPlaces(placename, selectoption, typeoftransport);
+    let selectoption = document.getElementById('selectoption').value;
+    console.log(selectoption);
+    let typeoftransport = document.getElementById('typeoftransport').value;
+    console.log();
+    new NewPlace(placename, selectoption, typeoftransport);
     setTOlocalstorig();
     renderTravel();
     console.log("ssss");
@@ -29,17 +31,24 @@ function addNewplaces(event) {
 function showListofPlace() {
     for (let i = 0; i < allPlaces.length; i++) {
         let viewallplace = document.getElementById('viewallplace');
-        let listOFplace = document.createElement('li');
+        let div = document.createElement('div')
+        let button = document.createElement('button');
+        button.innerHTML = 'X';
         let image = document.createElement('img');
+        let parg = document.createElement('p')
+        parg.style.display = "inline";
         image.style.display = "inline";
-        image.setAttribute('width', 140);
-        image.setAttribute('height', 140);
-        for (let j = 0; j < imageofPlace.length; j++) {
-            if (allPlaces[i].tplace === imageofPlace[j])
-                listOFplace.textContent = `${imageofPlace[j]}  Place Name:${allPlaces[i].pname}\nTrip Place:${allPlaces[i].tplace}\nTupe Of Transport:${allPlaces[i].toftransport}`;
-        }
-        viewallplace.appendChild(listOFplace);
-        listOFplace.appendChild(image);
+        image.style.margin = "20px";
+        image.setAttribute('width', 80);
+        image.setAttribute('height', 80);
+        image.src = "img/places/" + allPlaces[i].tplace + ".png"
+        parg.setAttribute('style', 'white-space: pre;');
+        parg.textContent = `Place Name:${allPlaces[i].pname}\r\nTrip Place:${allPlaces[i].tplace}\r\nTupe Of Transport:${allPlaces[i].toftransport}`;
+
+        viewallplace.appendChild(div)
+        div.appendChild(button);
+        div.appendChild(image);
+        div.appendChild(parg);
     }
 }
 
@@ -52,7 +61,12 @@ function getFromlocal() {
     allPlaces = JSON.parse(localStorage.getItem('travelinf')) || [];
 }
 function clearlist() {
-    allPlaces = []
+    // allPlaces = []
+    let viewallplace = document.getElementById('viewallplace');
+
+  while (viewallplace.hasChildNodes()) {
+    viewallplace.removeChild(viewallplace.firstChild);
+  }
 
 }
 console.log(allPlaces);
